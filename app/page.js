7a842e4297e9e2467cf606dd0443cb1dbcdfe83c
Javier Alvarado <jdrummer1@outlook.com>
2025-06-16 21:55:45 -0700

    Convert ol to p in page.js

--- app/page.js
+++ app/page.js
@@ -6,12 +6,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Hello World</h1>
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <p>Get started by editing <code>app/page.js</code>. Save and see your changes instantly.</p>
       </main>
     </div>
   );
