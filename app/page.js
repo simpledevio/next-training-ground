15442f1ba689630b1e2adec6d6d60841e0f0b886
Javier Alvarado <jdrummer1@outlook.com>
2025-06-18 21:47:23 -0700

    Delete extra code from app/page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div>
+      <main>
         <h1>Hello World</h1>
-        <p>Get started by editing <code>app/page.js</code>. Save and see your changes instantly.</p>
       </main>
     </div>
   );
